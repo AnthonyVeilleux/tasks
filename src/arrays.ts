c83f6217f85262cfe56b6dacf60d48a1bd54319d
Anthony Veilleux <anthony.veilleux@maine.edu>
2025-03-05 15:42:59 -0500

    Finished tripleNumbers

--- src/arrays.ts
+++ src/arrays.ts
@@ -27,7 +27,7 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    return numbers.map((numbers: number): number => numbers * 3);
 }
 
 /**
