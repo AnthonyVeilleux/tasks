12d4d6b566793a49c5ae002742e664d2a77876b4
Anthony Veilleux <anthony.veilleux@maine.edu>
2025-03-04 19:26:28 -0500

    Finished HTML CSS portion

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,60 @@
 import React from "react";
 import "./App.css";
+import image from "./assets/download.png"
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UM COS420 with React Hooks and TypeScript - Anthony Veilleux
+                      <header  className="App-header">
+                UM COS420 with React  and TypeScript - Anthony Veilleux
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
+            <Container>
+                <Row><div
+      style={{
+        width: "100px",
+        height: "100px",
+        backgroundColor: "red",
+      }}
+    />
+                    <Col><div
+      style={{
+        width: "100px",
+        height: "100px",
+        backgroundColor: "red",
+      }}
+    /></Col>
+                    <Col></Col>
+                    <div
+      style={{
+        width: "100px",
+        height: "100px",
+        backgroundColor: "red",
+      }}
+    />
+                <h1><div
+      style={{
+        width: "100px",
+        height: "100px",
+        backgroundColor: "red",
+      }}
+    /></h1>
+
+                </Row>
+            
+
+            <img src={image} alt="TypeScript Logo" />
+
+            <ul>
+            <li>First thing</li>
+            <li>Another thing</li>
+            <li>A third item</li>
+            return <div>
+        <Button onClick={ () => { console.log("Hello World!") } }>Log Hello World</Button>
+    </div>
+        </ul>Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
-            </p>
+            </Container>
         </div>
     );
 }
