443406eed8017c50f8cca9f77617e7730f81f8eb
Anthony Veilleux <anthony.veilleux@maine.edu>
2025-03-11 23:03:41 -0400

    finished makeAnswers

--- src/nested.ts
+++ src/nested.ts
@@ -59,7 +59,14 @@ export function getNames(questions: Question[]): string[] {
  * making the `text` an empty string, and using false for both `submitted` and `correct`.
  */
 export function makeAnswers(questions: Question[]): Answer[] {
-    return [];
+    return questions.map(function (q) {
+        return {
+            questionId: q.id,
+            text: "",
+            submitted: false,
+            correct: false,
+        };
+    });
 }
 
 /***
